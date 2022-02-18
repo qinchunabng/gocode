a00bdd284c71f08c89995c4ed5eb2857fd3f5092
qinchunabng <373413704@qq.com>
2022-02-18 12:33:13 +0800

    2022-2-18

--- src/project1/bool/app.go
+++ src/project1/bool/app.go
@@ -8,4 +8,14 @@ import (
 func main() {
 	var b = false
 	fmt.Println("b=", b, ",占用空间:", unsafe.Sizeof(b))
+
+	var age int = 40
+
+	if age > 30 {
+		fmt.Println("ok1")
+	}
+
+	if !(age > 30) {
+		fmt.Println("ok2")
+	}
 }
